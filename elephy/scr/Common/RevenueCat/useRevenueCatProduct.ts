23a0c378510d5251f32cc4fa7ee4f9485a93e6db
Minh Quy <onequy@gmail.com>
2024-10-23 07:24:21 +0700

    useRevenueCatProduct

--- elephy/scr/Common/RevenueCat/useRevenueCatProduct.ts
+++ elephy/scr/Common/RevenueCat/useRevenueCatProduct.ts
@@ -1,26 +1,58 @@
 // NUMBER [CHANGE HERE]: 0
 
-import { useEffect, useState } from "react"
-import { PurchasesStoreProduct } from "react-native-purchases"
-import { IAPProduct } from "../SpecificType"
-
 // Created Oct 2024 (coding Elephy)
 
 // Setup: RevenueCat.ts
 
-export const useRevenueCatProduct = (product: IAPProduct) => {
-    const [fetchedProduct, setFetchedProduct] = useState<PurchasesStoreProduct | undefined>()
+import { useCallback, useEffect, useMemo, useRef, useState } from "react"
+import { IAPProduct } from "../SpecificType"
+import { PRODUCT_CATEGORY, PurchasesStoreProduct } from "react-native-purchases"
+import { RevenueCat } from "./RevenueCat"
+
+export const useRevenueCatProduct = (targetProduct: IAPProduct | string, category?: PRODUCT_CATEGORY): {
+    fetchedAllProducts: undefined | PurchasesStoreProduct[],
+    fetchedTargetProduct: undefined | PurchasesStoreProduct,
+    localPrice: string | undefined,
+} => {
+    const [fetchedAllProducts, setFetchedAllProducts] = useState<PurchasesStoreProduct[]>([])
+    const fetchTimeout = useRef<NodeJS.Timeout | undefined>(undefined)
+
+    const fetchedTargetProduct = useMemo(() => {
+        if (targetProduct === undefined)
+            return undefined
+
+        if (fetchedAllProducts.length === 0)
+            return undefined
+
+        if (typeof targetProduct === 'string')
+            return fetchedAllProducts.find(i => i.identifier === targetProduct)
+        else
+            return fetchedAllProducts.find(i => i.identifier === targetProduct.sku)
+    }, [fetchedAllProducts, targetProduct])
+
+    const fetchListProducts = useCallback(async () => {
+        const items = await RevenueCat.FetchAllProductsWithCheckLocalCacheAsync(category)
+
+        if (Array.isArray(items))
+            setFetchedAllProducts(items)
+        else {
+            fetchTimeout.current = setTimeout(fetchListProducts, 1000)
+        }
+    }, [category])
 
     useEffect(() => {
         (async () => {
-            // const items = await RevenueCat.GetAllProductsWithCheckLocalCacheAsync([product])
-
-            // if (Array.isArray(items))
-            //     setFetchedProduct(items)
-
-            // console.log(items);
+            fetchListProducts()
         })()
+
+        return () => {
+            clearTimeout(fetchTimeout.current)
+        }
     }, [])
 
-    return fetchedProduct
+    return {
+        fetchedAllProducts,
+        fetchedTargetProduct,
+        localPrice: fetchedTargetProduct ? fetchedTargetProduct.priceString : undefined,
+    } as const
 }
