b003fb9f5f158d406d89554969ebbc6978e98d34
Minh Quy <onequy@gmail.com>
2024-10-23 18:46:26 +0700

    DevDevices

--- elephy/scr/Common/IsDev.ts
+++ elephy/scr/Common/IsDev.ts
@@ -11,9 +11,10 @@ var tapSetDevPersistenceCount = 0
 
 const DevDevices = [
     // '05073BCD-4B4A-4CF9-B4ED-055241D45573', // emulator iphone SE
+    // '8E3C624D-FD5F-4AF3-9DA7-F0D95C11DA98', // ?
     
     '2BD27B3C-F044-4AB6-A2F1-15F9F5172C7E', // iphone 6 timo
-    '8E3C624D-FD5F-4AF3-9DA7-F0D95C11DA98', // ip 15
+    '8A617553-4220-4DE9-813E-8A37F258317E', // ip 15
     
     '0ea1c473e9c9535d', // samsung s8
 ]
